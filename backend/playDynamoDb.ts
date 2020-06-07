3beccae2874eee8264a7a62fa8adf48bface2699
Jeffrey Wen <jeffrey@wenindoubt.com>
2020-06-07 04:21:28 -0700

    Remove Unused Resources

--- backend/playDynamoDb.ts
+++ backend/playDynamoDb.ts
@@ -3,37 +3,6 @@ import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 const ddb = new DocumentClient({ apiVersion: '2012-08-10' });
 
-export const getFacebookFrameworks: APIGatewayProxyHandler = async () => {
-  const params: DocumentClient.QueryInput = {
-    TableName: process.env.PLAY_TABLE,
-    KeyConditionExpression: '#PK = :PK',
-    ExpressionAttributeNames: {
-      '#PK': 'pk'
-    },
-    ExpressionAttributeValues: {
-      ':PK': 'ORG#7a64315f-3d52-4810-ad15-349fe0f83d95'
-    }
-  };
-
-  const { Items } = await ddb.query(params).promise();
-
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
-    },
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: Items,
-        path: 'HTTP GET -> API Gateway -> AWS lambda -> DynamoDB'
-      },
-      null,
-      2
-    )
-  };
-};
-
 export const getGoogleFrameworks: APIGatewayProxyHandler = async () => {
   const params: DocumentClient.QueryInput = {
     TableName: process.env.PLAY_TABLE,
